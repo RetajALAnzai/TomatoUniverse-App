f7c00787c0095cd47a7bc2bd5e2f43cdae941ab4
Anas Al Alwah <anasalalwah@gmail.com>
2025-11-04 14:14:58 +0300

    Initial Commit

---
+++ TomatoUniverseApp1/TomatoUniverseApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  TomatoUniverseApp1App.swift
+//  TomatoUniverseApp1
+//
+//  Created by Anas Alalwah on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct TomatoUniverseApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
